92a771c3c21449da90b194cb20870bf7b3110d21
Hojin <wkdghwls0623@gmail.com>
2024-03-13 00:12:29 +0900

    fist commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# CORS 미들웨어 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # 허용할 오리진
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메소드 허용
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
+
+
+
+@app.get("/hello")
+def hello():
+    return {"message": "안녕하세요 파이보"}
