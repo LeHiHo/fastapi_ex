4b40e377d6c9c2defa483d5c8d188c2f1e7cc9c4
Hojin <wkdghwls0623@gmail.com>
2024-03-23 10:31:14 +0900

    Feat: question_router 작성

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from domain.question import question_router
+
 app = FastAPI()
 
-# CORS 미들웨어 추가
+origins = [
+    "http://127.0.0.1:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # 허용할 오리진
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # 모든 HTTP 메소드 허용
-    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
 
-@app.get("/hello")
-def hello():
-    return {"message": "안녕하세요 파이보"}
+app.include_router(question_router.router)
