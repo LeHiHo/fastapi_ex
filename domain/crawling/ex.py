a0c1716e531fd0b93df85ee04e7fd0d2e1626123
Hojin <wkdghwls0623@gmail.com>
2024-04-23 01:54:01 +0900

    Fix: api 한도초과 수정, Feat: ali css선택자 변경

--- domain/crawling/ex.py
+++ domain/crawling/ex.py
@@ -28,8 +28,12 @@ def setup_driver():
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36")    
     options.add_argument("--disable-gpu")  
     # options.add_argument("--headless")
-    service = Service(GeckoDriverManager().install())
-    return webdriver.Firefox(service=service, options=options)
+    # service = Service(GeckoDriverManager().install()) # github api 한도초과남
+    # return webdriver.Chrome(service=service, options=options)
+
+
+    return webdriver.Firefox(options=options)
+
 
 def crawl_site(driver, url, find_all_args, name_args, price_args):
     driver.get(url)
@@ -63,21 +67,21 @@ def run_selenium(keyword):
     driver = setup_driver()
     try:
         coupang_url = f"https://www.coupang.com/np/search?component=&q={keyword}"        
-        aliExpress_url = f"https://ko.aliexpress.com/w/wholesale-{keyword}.html?spm=a2g0o.home.search.0"
+        aliExpress_url = f"https://ko.aliexpress.com/w/wholesale-{keyword}.html?spm=a2g0o.productlist.search.0"
         temu_url = f"https://www.temu.com/search_result.html?search_key={keyword}&search_method=user&refer_page_el_sn=200010&srch_enter_source=top_search_entrance_10005&_x_sessn_id=gb14fi35xs&refer_page_name=home&refer_page_id=10005_1713717575011_mqlwi0ykgm&refer_page_sn=10005"
         # amazon_url = f"https://www.amazon.com/s?k={keyword}&crid=27U3YQOWZ2RCZ&sprefix=us%2Caps%2C275&ref=nb_sb_noss_2"
         # never_url = f""
 
-        # coupang_products = crawl_site(driver, coupang_url, ['div', {'class': 'descriptions-inner'}], ['div', {'class': 'name'}], ['strong', {'class': 'price-value'}])
-        # aliExpress_products = crawl_site(driver, aliExpress_url, ['div', {'class': 'multi--title--G7dOCj3'}], ['h3'], ['div', {'class': 'multi--price-sale--U-S0jtj'}], ['span'])        
-        temu_products = crawl_site(driver,temu_url, ['div', {'class': '_6q6qVUF5 _1QhQr8pq _1ak1dai3 _3AbcHYoU'}], ['h2', {'class': '_2BvQbnbN'}], ['div', {'class': 'LiwdOzUs'}])
+        coupang_products = crawl_site(driver, coupang_url, ['div', {'class': 'descriptions-inner'}], ['div', {'class': 'name'}], ['strong', {'class': 'price-value'}])
+        aliExpress_products = crawl_site(driver, aliExpress_url, ['div', {'class': 'multi--content--11nFIBL'}], ['div', {'class': 'multi--title--G7dOCj3'}], ['div', {'class': 'multi--price-sale--U-S0jtj'}])        
+        # temu_products = crawl_site(driver,temu_url, ['div', {'class': '_6q6qVUF5 _1QhQr8pq _1ak1dai3 _3AbcHYoU'}], ['h2', {'class': '_2BvQbnbN'}], ['div', {'class': 'LiwdOzUs'}])
 
         # amazon_products = crawl_site(driver, amazon_url, ['div', {'data-component-type': 's-search-result'}], ['h2'], ['span', 'a-price'])
         # never_product = 
 
     finally:
         driver.quit()
-    return {'temu':temu_products}
+    return {'coupang':coupang_products, 'ali':aliExpress_products}
 
 @router.get("/")
 async def search(query: ProductQuery = Depends()):
