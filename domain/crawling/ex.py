a411b55dd71f383d3fcfb8f9854b9fd5b03cf00d
Hojin <wkdghwls0623@gmail.com>
2024-04-24 21:50:47 +0900

    remove: 안쓰는코드정리

--- domain/crawling/ex.py
+++ domain/crawling/ex.py
@@ -5,6 +5,8 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel, validator
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+# from selenium.webdriver.chrome.options import Options
+
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
@@ -25,8 +27,8 @@ class ProductQuery(BaseModel):
 def setup_driver():
     options = Options()
     options.headless = True
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36")    
-    options.add_argument("--disable-gpu")  
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36")
+    # options.add_argument("--disable-gpu")
     # options.add_argument("--headless")
     # service = Service(GeckoDriverManager().install()) # github api 한도초과남
     # return webdriver.Chrome(service=service, options=options)
@@ -37,7 +39,7 @@ def setup_driver():
 
 def crawl_site(driver, url, find_all_args, name_args, price_args):
     driver.get(url)
-    time.sleep(5) 
+    time.sleep(5)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     products = []
     for item in soup.find_all(*find_all_args):
@@ -47,41 +49,27 @@ def crawl_site(driver, url, find_all_args, name_args, price_args):
             products.append({'title': name.text.strip(), 'price': price.text.strip()})
     return products
 
-# def crawl_site(driver, url, find_all_args, name_args, price_container_args, price_parts_args):
-#     driver.get(url)
-#     time.sleep(5)  # 페이지 로드 대기
-#     soup = BeautifulSoup(driver.page_source, 'html.parser')
-#     products = []
-#     for item in soup.find_all(*find_all_args):
-#         name = item.find(*name_args).text.strip() if item.find(*name_args) else "No name found"
-#         price_container = item.find(*price_container_args)
-#         if price_container:
-#             price_parts = price_container.find_all(*price_parts_args)
-#             price = ''.join(part.text.strip() for part in price_parts)
-#         else:
-#             price = "No price found"
-#         products.append({'title': name, 'price': price})
-#     return products
-
 def run_selenium(keyword):
     driver = setup_driver()
     try:
         coupang_url = f"https://www.coupang.com/np/search?component=&q={keyword}"        
         aliExpress_url = f"https://ko.aliexpress.com/w/wholesale-{keyword}.html?spm=a2g0o.productlist.search.0"
-        temu_url = f"https://www.temu.com/search_result.html?search_key={keyword}&search_method=user&refer_page_el_sn=200010&srch_enter_source=top_search_entrance_10005&_x_sessn_id=gb14fi35xs&refer_page_name=home&refer_page_id=10005_1713717575011_mqlwi0ykgm&refer_page_sn=10005"
+        temu_url = f"https://www.temu.com/search_result.html?search_key=123%20c%EC%BB%AC%20%EC%86%8D%EB%88%88%EC%8D%B9&search_method=recent&refer_page_el_sn=200254&srch_enter_source=top_search_entrance_10132&_x_channel_scene=spike&_x_channel_src=1&_x_sessn_id=8e7j9npsxz&refer_page_name=lightning-deals&refer_page_id=10132_1713962421652_pgstqr10d5&refer_page_sn=10132"
         # amazon_url = f"https://www.amazon.com/s?k={keyword}&crid=27U3YQOWZ2RCZ&sprefix=us%2Caps%2C275&ref=nb_sb_noss_2"
         # never_url = f""
 
-        coupang_products = crawl_site(driver, coupang_url, ['div', {'class': 'descriptions-inner'}], ['div', {'class': 'name'}], ['strong', {'class': 'price-value'}])
-        aliExpress_products = crawl_site(driver, aliExpress_url, ['div', {'class': 'multi--content--11nFIBL'}], ['div', {'class': 'multi--title--G7dOCj3'}], ['div', {'class': 'multi--price-sale--U-S0jtj'}])        
-        # temu_products = crawl_site(driver,temu_url, ['div', {'class': '_6q6qVUF5 _1QhQr8pq _1ak1dai3 _3AbcHYoU'}], ['h2', {'class': '_2BvQbnbN'}], ['div', {'class': 'LiwdOzUs'}])
+        # coupang_products = crawl_site(driver, coupang_url, ['div', {'class': 'descriptions-inner'}], ['div', {'class': 'name'}], ['strong', {'class': 'price-value'}])
+        # aliExpress_products = crawl_site(driver, aliExpress_url, ['div', {'class': 'multi--content--11nFIBL'}], ['div', {'class': 'multi--title--G7dOCj3'}], ['div', {'class': 'multi--price-sale--U-S0jtj'}])
+        temu_products = crawl_site(driver,temu_url, ['div', {'class': '_6q6qVUF5 _1QhQr8pq _1ak1dai3 _3AbcHYoU'}], ['h2', {'class': '_2BvQbnbN'}], ['div', {'class': 'LiwdOzUs'}])
 
         # amazon_products = crawl_site(driver, amazon_url, ['div', {'data-component-type': 's-search-result'}], ['h2'], ['span', 'a-price'])
         # never_product = 
 
     finally:
         driver.quit()
-    return {'coupang':coupang_products, 'ali':aliExpress_products}
+
+    return {'temu': temu_products}
+
 
 @router.get("/")
 async def search(query: ProductQuery = Depends()):
